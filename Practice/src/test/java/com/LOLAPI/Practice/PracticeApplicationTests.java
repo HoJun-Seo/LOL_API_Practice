6b2c7d8cc02e1861599ae68fdd32cec955bf0a57
HoJun-Seo <ghwns6659@gmail.com>
2021-04-17 22:50:49 +0900

    LOL API Practice with Spring Boot

---
+++ Practice/src/test/java/com/LOLAPI/Practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LOLAPI.Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
